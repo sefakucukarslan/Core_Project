238fab416b465e129851f116c0f7452250732f5d
Sefa <sefak.arslan96@gmail.com>
2023-01-01 19:20:51 +0300

    Added Login Page

---
+++ BusinessLayer/Concrete/AnnouncementManager.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+        IAnnouncementDal _announcementDal;
+
+        public AnnouncementManager(IAnnouncementDal announcementDal)
+        {
+            _announcementDal = announcementDal;
+        }
+
+        public void TAdd(Announcement entity)
+        {
+            _announcementDal.Insert(entity);
+        }
+
+        public void TDelete(Announcement entity)
+        {
+            _announcementDal.Delete(entity);
+        }
+
+        public Announcement TGetByID(int id)
+        {
+           return _announcementDal.GetByID(id);
+        }
+
+        public List<Announcement> TGetList()
+        {
+            return _announcementDal.GetList();
+        }
+
+        public void TUpdate(Announcement entity)
+        {
+            _announcementDal.Update(entity);
+        }
+    }
+}
