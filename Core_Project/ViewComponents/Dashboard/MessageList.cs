2711f1a293d4d9aa6a6e13e894349461d73644b3
Sefa <sefak.arslan96@gmail.com>
2023-01-01 14:06:53 +0300

    Additions are being made to the board.

---
+++ Core_Project/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class MessageList : ViewComponent
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = messageManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
