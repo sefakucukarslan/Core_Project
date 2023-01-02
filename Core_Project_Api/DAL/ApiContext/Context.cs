85935a23eea6a27e10338a2bf694ac5424d97a40
Sefa <sefak.arslan96@gmail.com>
2023-01-02 18:02:08 +0300

    Added Api layer

---
+++ Core_Project_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using Core_Project_Api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Project_Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-ODEOOI6\\SQLEXPRESS;database=CoreProjectDB2;integrated security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
